045649dd59914b773e1356e39539c58f1214b472
tiffany1103 <1207939594@qq.com>
2019-07-03 14:31:17 +0800

    second

---
+++ src/pages/demo/Lify.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Child from './Child'
+
+export default   class Lify extends React.Component { 
+    constructor(props){
+         super(props)
+         this.state={
+             count:0
+         }
+    }
+
+    // state={
+    //     count:0
+    // }
+    handleAdd=()=>{
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+    handleClick(){
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+    render(){
+        // let style={
+        //     padding:50
+        // }
+        return(
+             <div style={{padding:50}}>
+                 <button onClick={this.handleAdd}>点击一下</button>
+                 <button onClick={this.handleClick.bind(this)}>点击一下</button>
+                 <p>{this.state.count}</p>
+                 <Child name={this.state.count}></Child>
+             </div>
+            ) 
+    }
+}
